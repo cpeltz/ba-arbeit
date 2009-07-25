ebddb1d979b0f2968e4e792fb55292b718726d16
Christoph Peltz <admin@keeg.de>
2009-07-25 12:11:57 +0200

    Cleaned up the debug sources and add documentation.

--- lib/debug.c
+++ lib/debug.c
@@ -1,27 +1,36 @@
-/*#define __AVR_ATmega2561__*/
 #include <stdlib.h>
 #include <avr/pgmspace.h>
 #include "uart.h"
 #include "debug.h"
 #include "flags.h"
-#include "definitions.h"
 
+/**
+ * @defgroup DEBUG_MODULE Debug Module
+ * Module used to print debug information on the UART device.
+ * @{
+ */
+
+/**
+ * Buffer used by debug_WriteInteger and the itoa call.
+ */
 static char buffer[10];
 
+/**
+ * Prints one character of a debug string.
+ *
+ * Prints only if DEBUG_ENABLE flag is asserted.
+ * @param[in] data The character to be printed.
+ */
 void debug_PutChar(const uint8_t data) {
 	if (flag_read(DEBUG_ENABLE))
 		uart_put_debug(data);
 }
 
-void debug_PutChar_P(const uint8_t * data) {
-	if (flag_read(DEBUG_ENABLE))
-		debug_PutChar(*data);
-}
-
-static void debug_SendESC(void) {
-	debug_PutChar(27);
-}
-
+/**
+ * Prints a String for debugging purposes.
+ *
+ * @param[in] data The String to be printed
+ */
 void debug_PutString(const char *data) {
 	uint8_t character;
 
@@ -30,52 +39,50 @@ void debug_PutString(const char *data) {
 	}
 }
 
+/**
+ * Prints a String saved in the Program-Memory.
+ *
+ * @param[in] progmem_string The Program-Memory string to be printed.
+ */
 void debug_WriteString_P(const char *progmem_string) {
 	uint8_t character;
 
 	while ((character = pgm_read_byte(progmem_string++))) {
-		debug_PutChar(character);
+		debug_PutChar(*character);
 	}
 }
 
+/**
+ * Helper function used by debug_ClearEOL() to send escape sequences.
+ *
+ * @param[in] sequence The Sequence which should be transmitted.
+ */
 void debug_SendESCSequence(const char *sequence) {
 	debug_SendESC();
 	debug_PutChar('[');
 	debug_PutString(sequence);
 }
 
-void debug_ResetTerminal(void) {
-	debug_SendESC();
-	debug_PutChar('c');
-}
-
-void debug_ClearScreen(void) {
-	debug_SendESCSequence("2J");
-}
-
-void debug_GotoXY(const uint8_t x, const uint8_t y) {
-	itoa(y, buffer, 10);
-	debug_SendESCSequence(buffer);
-	debug_PutChar(';');
-	itoa(x, buffer, 10);
-	debug_PutString(buffer);
-	debug_PutChar('H');
-}
-
-void debug_CursorUp(const uint8_t y) {
-	itoa(y, buffer, 10);
-	debug_SendESCSequence(buffer);
-	debug_PutChar('A');
-}
-
+/**
+ * Helper function for debug_WriteInteger(). Sends an end-of-line.
+ */
 void debug_ClearEOL(void) {
 	debug_SendESCSequence("K");
 }
 
+/**
+ * Helper function for debug_WriteInteger(). Sends an new-line.
+ */
 void debug_NewLine(void) {
 	debug_WriteString_P(PSTR("\n"));
 }
 
+/**
+ * Prints a program-memory string in conjunction with a integer value.
+ *
+ * @param[in] progmem_message The Message in program-memory.
+ * @param[in] integer The integer which will printed alongside the message.
+ */
 void debug_WriteInteger(const char *progmem_message, const int16_t integer) {
 	itoa(integer, buffer, 10);
 	debug_WriteString_P(progmem_message);
@@ -83,3 +90,4 @@ void debug_WriteInteger(const char *progmem_message, const int16_t integer) {
 	debug_ClearEOL();
 	debug_NewLine();
 }
+/*@}*/
--- lib/debug.h
+++ lib/debug.h
@@ -3,21 +3,10 @@
 
 #include <inttypes.h>
 
-/* Mostly unchanged. I took this part from the old Driver.
- * It's used to print out debugging Information.
- */
-
 void debug_PutChar(const uint8_t data);
 void debug_PutChar_P(const uint8_t * data);
 void debug_PutString(const char *data);
 void debug_WriteString_P(const char *progmem_string);
-void debug_SendESCSequence(const char *sequence);
-void debug_ResetTerminal(void);
-void debug_ClearScreen(void);
-void debug_GotoXY(const uint8_t x, const uint8_t y);
-void debug_ClearEOL(void);
-void debug_CursorUp(const uint8_t y);
-void debug_NewLine(void);
 void debug_WriteInteger(const char *message, const int16_t integer);
 
 #endif
